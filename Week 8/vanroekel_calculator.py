a43c2a8b0f12084ef5d3212ea426b029b6bd46d8
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-12-05 15:09:30 -0600

    Week 8 assignments

---
+++ Week 8/vanroekel_calculator.py
@@ -0,0 +1,22 @@
+
+#============================================
+# Title:  vanroekel_calculator.py
+# Author: Richard Krasso
+# Modified by: Faye Van Roekel
+# Date:   05 December 2019
+# Description: Exercise 8.3
+#===========================================
+
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1/num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
